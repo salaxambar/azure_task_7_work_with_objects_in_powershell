2b5be54f3a3706e4ffe006f75230732f873adef9
Marko Panchak <marko.panchak1@gmail.com>
2024-08-10 13:38:04 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,11 @@
-# Write your code here
+$region = @()
+$files = Get-ChildItem -Path ./data -File
+foreach ($file in $files) {
+  $vm = Get-Content $file.FullName | ConvertFrom-Json
+
+  if ($vm.Name -contains "Standard_B2pts_v2") {
+    $regionName = $file.Name.Replace('.json', '')
+    $region += $regionName
+  }
+}
+$region | ConvertTo-Json | Out-File -FilePath "./result.json"
